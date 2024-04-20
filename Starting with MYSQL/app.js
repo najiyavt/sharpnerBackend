124fdbed54d043b0cc82573af9b4aee0e6245991
najiyavt <najiyavtibrahim@gmail.com>
2024-04-20 13:49:37 +0530

    intro to MYSQL

--- Starting with MYSQL/app.js
+++ Starting with MYSQL/app.js
@@ -1,16 +1,31 @@
-const mysql=require('mysql2');
+const path = require('path');
 
-const pool=mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: 'SUresh@1289'
-});
+const express = require('express');
+const bodyParser = require('body-parser');
 
-let db=pool.promise();
+const errorController = require('./controllers/error');
+const db = require('./util/database')
+const app = express();
 
-db.query('select * from products').then((result)=>{
-    console.log(result);
-}).catch((err)=>{
-    console.log(err);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+.then(result => {
+    console.log(result[0],result[1])
 })
+.catch(err => {
+    console.log(err);
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
